70636dfd82f9329c9b1c3128d3a48be3226a829d
Marco Bruno <marco.bruno.br@gmail.com>
2017-02-25 20:54:06 -0300

    Created response to route /

--- app/server.js
+++ app/server.js
@@ -1,13 +1,16 @@
-let http = require('http')
-let port = 3000
-let ip = 'localhost'
+  let http = require('http')
+  let port = 3000
+  let ip = 'localhost'
 
-let server = http.createServer((req, res) => {
-  console.log('Recebendo uma request!')
-  res.end('<h1>Aqui fica o que vamos enviar para o navegador como resposta!</h1>')
-})
+  let server = http.createServer((req, res) => {
+    if (req.url == '/') {
+      res.end('<h1>Home</h1>')
+    }
 
-server.listen(port, ip, () => {
-  console.log(`Servidor rodando em http://${ip}:${port}`)
-  console.log('Para derrubar o servidor: ctrl + c');
-})
+    res.end('<h1>URL sem resposta definida!</h1>')
+  })
+
+  server.listen(port, ip, () => {
+    console.log(`Servidor rodando em http://${ip}:${port}`)
+    console.log('Para derrubar o servidor: ctrl + c');
+  })
